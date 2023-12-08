5bafd6a563c91b4e039106a053477afeb4248ccb
jck <jckleinbourg@gmail.com>
2023-12-08 13:30:17 +0100

    Ajoute fragment navbar

--- src/main/java/com/ipi/jva320/web/SalarieController.java
+++ src/main/java/com/ipi/jva320/web/SalarieController.java
@@ -1,16 +1,15 @@
 package com.ipi.jva320.web;
 
+import com.ipi.jva320.exception.SalarieException;
 import com.ipi.jva320.model.SalarieAideADomicile;
 import com.ipi.jva320.service.SalarieAideADomicileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.logging.Logger;
 
 //@ResponseStatus
@@ -20,20 +19,36 @@ public class SalarieController {
 
     @Autowired
     SalarieAideADomicileService salarieAideADomicileService;
-    @GetMapping(value = "/salaries/{id}")
+
+    @GetMapping("/salaries")
+    public String getSalariesList(final ModelMap model){
+        return "list";
+    }
+    @GetMapping("/salaries/{id}")
     public String getSalaries(final ModelMap model, @PathVariable long id) {
         SalarieAideADomicile salarie = salarieAideADomicileService.getSalarie(id);
         model.put("salarie", salarie);
         return "detail_Salarie";
     }
-
-    //@PostMapping(value = "/salaries/")
-
-    @PostMapping(value = "/salaries/new")
-    public String createSalarie() {
+    @PostMapping("/salaries/save")
+    public String modifySalarie(@ModelAttribute SalarieAideADomicile salarieAideADomicile, final ModelMap model) throws SalarieException {
+        try {
+            salarieAideADomicileService.updateSalarieAideADomicile(salarieAideADomicile);
+        } catch (SalarieException e){
+            salarieAideADomicileService.creerSalarieAideADomicile(salarieAideADomicile);
+        }
+        model.put("salarie", salarieAideADomicile);
         return "detail_Salarie";
     }
+    @PostMapping("salaries/new")
+    public void createSalarie(final ModelMap model){
+        System.out.println(">>>>>>>>>>>>>> création");
+    }
+
 }
+
+
+
 /*
 @ResponseStatus(value = HttpStatus.NO_CONTENT)
 public class SalarieInvalideException() extends Exception{
