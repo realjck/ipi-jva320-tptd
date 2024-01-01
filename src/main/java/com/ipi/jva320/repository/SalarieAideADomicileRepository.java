7b68bd42c7648c4d44d6513b1d1d060ecd48164c
realjck <jckleinbourg@gmail.com>
2024-01-01 19:40:59 +0100

    Finalisation exercice

--- src/main/java/com/ipi/jva320/repository/SalarieAideADomicileRepository.java
+++ src/main/java/com/ipi/jva320/repository/SalarieAideADomicileRepository.java
@@ -2,7 +2,6 @@ package com.ipi.jva320.repository;
 
 import com.ipi.jva320.model.SalarieAideADomicile;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
